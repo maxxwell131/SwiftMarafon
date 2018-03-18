8083ae7ecc6e864012a4fb3c334bea49bb6705de
maxxwel131 <maxxwell131@yahoo.com>
2018-03-18 15:36:53 +0200

    Less 10, task 5 finish

--- Less_10.playground/Contents.swift
+++ Less_10.playground/Contents.swift
@@ -1,3 +1,10 @@
+import Foundation
+
+extension String {
+    mutating func replace(_ originalString:String, with newString:String) {
+        self = self.replacingOccurrences(of: originalString, with: newString)
+    }
+}
 /*
  1. Создайте пару функций с короткими именами, которые возвращают строку с классным символом или символами. Например heart() возвращает сердце и т.п. Вызовите все эти функции внутри принта для вывода строки этих символов путем конкатенации.
  */
@@ -22,16 +29,16 @@ print(printPhone() + printHeart() + printEuro())
 func ColorOfCell(symbolCell: String) -> String? {
     let colCellArray = ["a","b","c","d","e","f","g","h"]
     let row = Int(String(symbolCell.last!))
-    var col: String = String(symbolCell.lowercased().first!)
+    let col: String = String(symbolCell.lowercased().first!)
     var cellColor:Bool = false
-    var colStrintToNumber:Int? = 1 + colCellArray.index(of: String(col))!
+    let colStrintToNumber:Int? = 1 + colCellArray.index(of: String(col))!
     
     if (row! + colStrintToNumber!) % 2 == 0  {
         cellColor = true
     } else {
         cellColor = false
     }
-    var result = cellColor ? "белая": "черная"
+    let result = cellColor ? "белая": "черная"
     return result
 }
 
@@ -70,44 +77,33 @@ print(inputArray_4)
 /*
 5. Создайте функцию, которая принимает строку, убирает из нее все знаки препинания, делает все гласные большими буквами, согласные маленькими, а цифры меняет на соответствующие слова (9 -> nine и тд)
  */
-func editToString(inputString: String) {
+func editToString(inputString: inout String) {
     let vovelsArray = ["а","о","и","е","ё","э","ы","у","ю","я"] //Гласные буквы
     let consonantsArray = ["б","в","г","д","ж","з","й","к","л","м","н","п","р","с","т","ф","х","ц","ч","ш","щ"] //Согласные буквы
     let numbersDictonary = ["0":"ноль","1":"один","2":"два","3":"три","4":"четыре","5":"пять","6":"шесть","7":"семь","8":"восемь","9":"девять"] //цифры
     let signsArray = [".","?","!","-",","] //знаки препинания
     var myCharsterArray = Array(inputString)
     
-    var coubters = (vovels: 0, consonants: 0, numbs: 0, signs: 0)
-    
-    
     for item in 0..<myCharsterArray.count {
-        
-//        let tmp = numbersDictonary.index(forKey: String(myCharsterArray[item]))
-//        print(tmp)
-        
         if let _ = consonantsArray.index(of: (String(myCharsterArray[item])).lowercased()) { //Согласные маленькими буквы
-            //coubters.consonants += 1
             myCharsterArray[item] = Character(String(myCharsterArray[item]).lowercased())
         } else if let _ = vovelsArray.index(of: (String(myCharsterArray[item])).lowercased()) { //Гласные большими буквам
-            //coubters.vovels += 1
             myCharsterArray[item] = Character(String(myCharsterArray[item]).uppercased())
-        } else if let indexDict = (numbersDictonary.index(forKey: String(myCharsterArray[item]))) { //цифрыцифры меняет на соответствующие слова index(forke: )
-            coubters.numbs += 1
-            print("key:\(numbersDictonary[indexDict].key) value:\(numbersDictonary[indexDict].value)")
-            //myCharsterArray[item] = Character(String(numbersDictonary[String(myCharsterArray[item])]))
-            // let tmp = numbersDictonary.index(forKey: String(myCharsterArray[item]))
         } else if let _ = signsArray.index(of: (String(myCharsterArray[item])).lowercased()) { //убрать знак препинания
-            //coubters.signs += 1
             myCharsterArray[item] = Character(String(" "))
         }
     }
-    print("Согласные буквы -\(coubters.consonants)\rГласные буквы - \(coubters.vovels)\rКол-во символов - \(myString.count)\rцифр - \(coubters.numbs)\rЗнаки припенания - \(coubters.signs) ")
-    
-    print(String(myCharsterArray))
 
+    inputString = String(myCharsterArray)
+    
+    for (key, _ ) in numbersDictonary {
+        inputString = inputString.replacingOccurrences(of: key, with: numbersDictonary[key]!)
+    }
+    
 }
 
 
-let myString = "1 Представители Google также сообщили, что сегодня считают одной из основных своих задач удаление вредоносной или назойливой рекламы. В 2017 году компания посчитала некорректными, обманывающими или вредоносными 3,2 млрд рекламных объявлений в Сети. Это почти вдвое больше, чем 1,7 млрд объявлений, которые Google счел такими в 2016 году. Такими объявлениями Google, в частности, считает ссылки, которые ведут на сайты с вирусами (таких компания насчитала 79 млн штук)."
+var myString = "1 Представители Google также сообщили, что сегодня считают одной из основных своих задач удаление вредоносной или назойливой рекламы. В 2017 году компания посчитала некорректными, обманывающими или вредоносными 3,2 млрд рекламных объявлений в Сети. Это почти вдвое больше, чем 1,7 млрд объявлений, которые Google счел такими в 2016 году. Такими объявлениями Google, в частности, считает ссылки, которые ведут на сайты с вирусами (таких компания насчитала 79 млн штук)."
+editToString(inputString: &myString)
+print(myString)
 
-editToString(inputString: myString)
