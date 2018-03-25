0a55993dc97864b00d56c18e5e245cc5ce5206f6
maxxwel131 <maxxwell131@yahoo.com>
2018-03-25 18:39:52 +0300

    Less 12 task 3 50%

--- Less_12.playground/Contents.swift
+++ Less_12.playground/Contents.swift
@@ -36,10 +36,10 @@ let whiteKing: Piece = .King(name: .King, color: .White,positionX: .x("A"), posi
 let blackKing: Piece = .King(name: .King, color: .Black, positionX: .x("A"), positionY: .y(2))
 let blackCastle: Piece = .Castle(name: .Castle, color: .Black, positionX: .x("B"), positionY: .y(1))
 
-let chessboard = [ whiteKing, blackKing, blackCastle]
+let chessboard: [Piece] = [ whiteKing, blackKing, blackCastle]
 
 func showPiece(piece: Piece) -> String{
-    var result = String("--")
+    var result = String()
     
     switch piece {
     case .King(let name, let color, let posX, let posY):
@@ -64,7 +64,51 @@ for obj in chessboard {
 
 /*
  3. Используя красивые юникодовые представления шахматных фигур, выведите в консоли вашу доску. Если клетка не содержит фигуры, то используйте белую или черную клетку. Это должна быть отдельная функция, которая распечатывает доску с фигурами (принимает массив фигур и ничего не возвращает)
+ 
+ print(" \u{2B1C} \u{2B1B}")
+ print(" \u{265A} \u{2654} ")
+ print(" \u{265B} \u{2655} ")
+ print(" \u{265C} \u{2656} ")
+ print(" \u{265D} \u{2657} ")
+ print(" \u{265E} \u{2658} ")
+ print(" \u{265F} \u{2659} ")
 */
+
+
+//MARK: функция печатает на экран доску и выводит на нее фигуры
+func showChessboard (board: [Piece]) {
+    for obj in board {
+        print(drawPiece(piece: obj))
+    }
+}
+
+//MARK: функция принимает фигуру, расшифровывает и возвращает рисинок Unicode
+func drawPiece(piece: Piece) -> String{
+    var result = String()
+    
+    switch piece {
+    case .King( _, let color, _,  _):
+        result = (color == .White) ? "\u{265A}" : "\u{2654}"
+    case .Queen( _, let color, _, _):
+        result = (color == .White) ?  "\u{265B}" : "\u{2655}"
+    case .Bishop( _, let color, _, _):
+        result = (color == .White) ? "\u{265D}" : "\u{2657}"
+    case .Knight( _, let color, _, _):
+        result = (color == .White) ?  "\u{265E}" : "\u{2658}"
+    case .Castle( _, let color, _, _):
+        result = (color == .White) ? "\u{265C}" : "\u{2656}"
+    case .Pawn( _, let color, _, _):
+        result = (color == .White) ? "\u{265F}" : "\u{2659}"
+    }
+    return result
+}
+
+showChessboard(board: chessboard)
+
+
+func colorOfCell() {
+    
+}
 
 /*
  4. Создайте функцию, которая будет принимать шахматную фигуру и тюпл новой позиции. Эта функция должна передвигать фигуру на новую позицию, причем перемещение должно быть легальным: нельзя передвинуть фигуру за пределы поля и нельзя двигать фигуры так, как нельзя их двигать в реальных шахматах (для мегамонстров программирования). Вызовите эту функцию для нескольких фигур и распечатайте поле снова.
