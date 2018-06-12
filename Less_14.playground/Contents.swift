acb87c6cd08ac73dbd8edf1cc0d888f301f783ce
MaxxWell <maxxwell131@yahoo.com>
2018-06-12 22:38:20 +0300

    less 14 1-80%

--- Less_14.playground/Contents.swift
+++ Less_14.playground/Contents.swift
@@ -1,5 +1,52 @@
+import Foundation
+
 //1. Самостоятельно повторить проделанное в уроке
 
+struct Student {
+    var firstName: String {
+        willSet {
+            print("Will set " + newValue + " insted of " + firstName)
+        }
+        
+        didSet {
+            print("did set " + firstName + " insted of " + oldValue)
+            firstName = firstName.capitalized
+        }
+    }
+    
+    var lastName: String {
+        didSet {
+            lastName = lastName.capitalized
+        }
+    }
+    
+    var fullName: String {
+        get {
+            return firstName + " " + lastName
+        }
+        
+        set {
+            print("fullName wonts to be set to " + newValue)
+            let words = newValue.components(separatedBy: " ")
+            
+            if words.count > 0 {
+                firstName = words[0]
+            }
+            
+            if words.count > 1 {
+                lastName = words[1]
+            }
+        }
+        
+        init(firstName: String, lastName: String) {
+            
+        }
+    }
+    
+    var student = Student()
+
+    
+}
 //2. Добавить студенту property «Дата рождения» (пусть это будет еще одна структура, содержащая день, месяц, год) и два computed property: первое — вычисляющее его возраст, второе — вычисляющее, сколько лет он учился (считать, что он учился в школе с 6 лет, если студенту меньше 6 лет — возвращать 0)
 
 //3. Создать структуру «Отрезок», содержащую две внутренние структуры «Точки». Структуру «Точка» создать самостоятельно, несмотря на уже имеющуюся в Swift’е. Таким образом, структура «Отрезок» содержит две структуры «Точки» — точки A и B (stored properties). Добавить два computed properties: « середина отрезка» и «длина» (считать математическими функциями)
